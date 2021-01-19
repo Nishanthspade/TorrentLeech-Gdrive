e1e9ee860e201516b3b6e683e9ca2e0388db7e6d
Nishanthspade <47189779+Nishanthspade@users.noreply.github.com>
2021-01-19 15:30:48 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,7 +1,17 @@
 from tobrot.sample_config import Config
 
+#read readme too before filling these stuffs
+
 class Config(Config):
-  TG_BOT_TOKEN = "1535821528:AAEGZ12CxEpQVn6tKyPJEzqs-5Z1Avsnh30"
-  APP_ID = 2442994
-  API_HASH = "7e478984273964dc9e787b984d7e9c8c"
-  AUTH_CHANNEL = [-1001309737901]
+    TG_BOT_TOKEN= "10your2910:AAE-IeBloaZ-your-hmhQXqLQ" #imp
+    APP_ID = 2442994 #imp
+    API_HASH = "7e478984273964dc9e787b984d7e9c8c" #imp
+    AUTH_CHANNEL = [-1001309737901] #imp replace your_id with your id from telegram or delete
+    AUTH_OWNER = [720679955]
+    GLEECH_COMMAND = "gleech@spade_leechbot"
+    YTDL_COMMAND = 'ytdl@spade_leechbot'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@spade_leechbot"
+    CLONE_COMMAND_G = "gclone@groupname"
+    PYTDL_COMMAND_G = "pytdl@groupname"
+    DESTINATION_FOLDER = "Spade-drive"
+    LEECH_COMMAND = "leech@spade_leechbot"
