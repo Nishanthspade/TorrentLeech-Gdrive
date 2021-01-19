e1be94e961a91b6568ed13ba8ddf8ee9a24913ff
Nishanthspade <47189779+Nishanthspade@users.noreply.github.com>
2021-01-19 14:10:14 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1535821528:AAEGZ12CxEpQVn6tKyPJEzqs-5Z1Avsnh30"
+  APP_ID = 2442994
+  API_HASH = "7e478984273964dc9e787b984d7e9c8c"
+  AUTH_CHANNEL = [-1001309737901]
